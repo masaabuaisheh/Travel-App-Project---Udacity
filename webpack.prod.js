77e3c90a7f1a4277bad1a55325733eb39f7a5563
masaabuaisheh <masaabuaisheh11@gmail.com>
2024-09-02 11:11:58 +0300

    add new features

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,14 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin"),
+const common = require("./webpack.common.js"),
+  { merge } = require("webpack-merge"),
+  CssMinimizerPlugin = require("css-minimizer-webpack-plugin"),
   TerserPlugin = require("terser-webpack-plugin"),
-  path = require("path"),
-  common = require("./webpack.common.js");
-const { merge } = require("webpack-merge"),
-  CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+  MiniCssExtractPlugin = require("mini-css-extract-plugin"),
+  WorkboxPlugin = require("workbox-webpack-plugin"),
+  path = require("path");
 
 module.exports = merge(common, {
   mode: "production",
-  devtool: "hidden-source-map",
+  devtool: "source-map",
   module: {
     rules: [
       {
@@ -17,24 +18,25 @@ module.exports = merge(common, {
     ],
   },
   output: {
-    filename: "bundle.[contenthash].js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
     libraryTarget: "var",
     library: "Client",
     clean: true,
   },
   optimization: {
-    minimize: true,
     minimizer: [
-      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
-      // `...`,
+      `...`, // Keep existing minimizers like TerserPlugin
       new CssMinimizerPlugin(),
-      new TerserPlugin(),
     ],
+    minimize: true,
   },
   plugins: [
-    new MiniCssExtractPlugin({
-      filename: "style.[contenthash].css",
+    new MiniCssExtractPlugin({ filename: "[name].css" }),
+    // Add the WorkboxPlugin to generate the service worker
+    new WorkboxPlugin.GenerateSW({
+      clientsClaim: true,
+      skipWaiting: true,
     }),
   ],
 });
